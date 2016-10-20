ff1206f90297ce1ee85ef594ae45a066b1ec880b
Andres Leon <andres.leon.job@gmail.com>
2016-10-20 17:54:06 +0200

    Add authentication through savon web services

---
+++ app/services/uweb_authenticator.rb
@@ -0,0 +1,50 @@
+class UwebAuthenticator
+  
+  def initialize(data={})
+    @user_params = { login: data[:login] }.with_indifferent_access
+  end
+
+  def authenticate!
+    return false unless @user_params[:login]
+    return true if user_exists?
+    false
+  end
+
+  private
+
+    def user_exists?
+      response = client.call(:get_user_data_by_login, message: { ub: {login: @user_params[:login]} }).body
+      parsed_response = parser.parse(response[:get_user_data_by_login_response][:get_user_data_by_login_return])
+      @uweb_user = uweb_user_data(parsed_response)
+      @user_params[:login] == @uweb_user[:login]
+    rescue  Exception  => e
+      Rails.logger.error('UwebAuthenticator#user_exists? ') do
+        "Error llamada UWEB: get_user_data_by_login - #{@user_params}. Error: #{e}"
+      end
+      false
+    end
+
+    def uweb_user_data(parsed_response)
+      user_data = parsed_response.fetch('USUARIO')
+      {
+        login:             user_data['LOGIN'],
+        uweb_id:           user_data['CLAVE_IND'],
+        name:              user_data['NOMBRE_USUARIO'],
+        surname:           user_data['APELLIDO1_USUARIO'],
+        second_surname:    user_data['APELLIDO2_USUARIO'],
+        document:          user_data['DNI'],
+        phone:             user_data['TELEFONO'],
+        email:             user_data['MAIL'],
+        official_position: user_data['CARGO'],
+        personal_number:   user_data['NUM_PERSONAL']
+      }
+    end
+
+    def client
+      @client ||= Savon.client(wsdl: Rails.application.secrets.uweb_wsdl, raise_errors: true)
+    end
+
+    def parser
+      @parser ||= Nori.new
+    end
+end 
