489856b5a4ddc52e9ce1c88fd220860f16124ad2
Andres Leon <andres.leon.job@gmail.com>
2016-12-07 13:10:44 +0100

    Add deploy.rb

---
+++ config/deploy.rb
@@ -0,0 +1,60 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+# TODO Remove this line when cap -T works
+set :stage, 'preproduction'
+
+def deploysecret(key)
+  @deploy_secrets_yml ||= YAML.load_file('config/deploy_secrets.yml')[fetch(:stage).to_s]
+  @deploy_secrets_yml[key.to_s]
+end
+
+set :rails_env, fetch(:stage)
+set :rvm_ruby_version, '2.2.2'
+set :rvm_type, :user
+
+set :application, 'volun_backend'
+set :server_name, deploysecret(:server_name)
+set :full_app_name, fetch(:application)
+# If ssh access is restricted, probably you need to use https access
+set :repo_url, 'https://perezljl:Albarracin33@bitbucket.org/perezljl/volun_backend.git'
+
+set :scm, :git
+set :revision, `git rev-parse --short #{fetch(:branch)}`.strip
+
+set :log_level, :info
+set :pty, true
+set :use_sudo, false
+
+set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_dirs, %w{log tmp public/system public/assets}
+
+set :keep_releases, 10
+
+set :local_user, ENV['USER']
+
+# Run test before deploy
+set :tests, ["spec"]
+
+# Config files should be copied by deploy:setup_config
+set(:config_files, %w(
+  log_rotation
+  database.yml
+  secrets.yml
+  unicorn.rb
+))
+
+
+namespace :deploy do
+  # Check right version of deploy branch
+  before :deploy, "deploy:check_revision"
+  # Run test aund continue only if passed
+  # before :deploy, "deploy:run_tests"
+  # Compile assets locally and then rsync
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
+  # Restart unicorn
+  after 'deploy:publishing', 'deploy:restart'
+  # after 'deploy:restart', 'sidekiq:restart'
+end
+
