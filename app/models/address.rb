b2d5d991e188118350d1b1a2abfdc516d87fa137
Andres Leon <andres.leon.job@gmail.com>
2016-12-13 08:48:06 +0100

    Check addresss norimalization before validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ActiveRecord::Base
 
+  attr_accessor :bdc_validator
+
   ROAD_NUMBER_TYPES = %w(num km.)
   GRADERS = [*'A'..'Z']
 
@@ -7,6 +9,38 @@ class Address < ActiveRecord::Base
   belongs_to :road_type
   belongs_to :province
 
-  validates :road_type, :road_name, :road_number, :postal_code, :province, :country, presence: true
+  validates :road_type_id, :road_name, :road_number, :postal_code, :province, :country, :town, presence: true
+  after_validation :check_normalization
 
+  def normalized?
+    ndp_code.present? || bdc_validator.address_normalized?
+  end
+
+  def bdc_validator
+    self.bdc_validator ||= BdcValidator.new(bdc_fields)
+  end
+
+  private
+
+  def check_normalization
+    self.ndp_code ||= bdc_validator.ndp_code if normalized?
+  end
+
+  def bdc_fields
+    {
+        country:          country,
+        province:         province.name,
+        town:             town,
+        road_type:        road_type.name,
+        road_name:        road_name,
+        road_number_type: road_number_type,
+        road_number:      road_number,
+        grader:           grader,
+        stairs:           stairs,
+        floor:            floor,
+        door:             door,
+        bdc_exchange:     '',
+        aplication:       ''
+    }
+  end
 end
