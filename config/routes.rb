70519d5f4fb5ff4fb07f757383b3a77918a1d767
Andres Leon <andres.leon.job@gmail.com>
2016-12-14 12:58:49 +0100

    Delete rft* files. Rename project_fields partial andselect_pt_extensions view

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,19 @@ Rails.application.routes.draw do
 
 
 
-  resources :rft_others
-  resources :rft_activity_unpublishings
-  resources :rft_activity_publishings
-  resources :rft_project_unsubscribes
-  resources :rft_project_unpublishings
-  resources :rft_project_publishings
-  resources :rft_volunteers_demands
-  resources :rft_entity_unsubscribes
-  resources :rft_entity_subscribes
-  resources :rft_volunteer_appointments
-  resources :rft_volunteer_amendments
-  resources :rft_volunteer_unsubscribes
-  resources :rft_volunteer_subscribes
+  resources :rt_others
+  resources :rt_activity_unpublishings
+  resources :rt_activity_publishings
+  resources :rt_project_unsubscribes
+  resources :rt_project_unpublishings
+  resources :rt_project_publishings
+  resources :rt_volunteers_demands
+  resources :rt_entity_unsubscribes
+  resources :rt_entity_subscribes
+  resources :rt_volunteer_appointments
+  resources :rt_volunteer_amendments
+  resources :rt_volunteer_unsubscribes
+  resources :rt_volunteer_subscribes
   resources :request_forms
   resources :request_form_reasons
   resources :request_form_types
@@ -24,11 +24,6 @@ Rails.application.routes.draw do
   resources :timetables
   resources :districts
   resources :addresses
-  resources :pt_others
-  resources :pt_socials
-  resources :pt_centres
-  resources :pt_permanents
-  resources :pt_punctuals
   resources :pt_entities
   resources :pt_subventions
   resources :links
@@ -48,7 +43,7 @@ Rails.application.routes.draw do
     post :recover, on: :member
   end
   resources :projects, concerns: :recoverable do
-    get 'new_options', on: :collection
+    get 'select_pt_extensions', on: :collection
   end
   resources :project_types, concerns: :recoverable
   resources :coordinations, concerns: :recoverable
@@ -62,56 +57,5 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  # root 'proposals#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
 
 end
