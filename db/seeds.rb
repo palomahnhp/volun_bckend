f4bfd842ae58374626965add1cfd183d1bb8927a
Andres Leon <andres.leon.job@gmail.com>
2016-12-21 18:55:29 +0100

    Add temporary seeds content

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,306 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Faker::Config.locale = I18n.locale
 
+PROJECTS_NUM      = 10
+DISTRICTS_NUM     = 10
+ACTIVITIES_NUM    = 10
+ENTITIES_NUM      = 10
+COORDINATIONS_NUM = 10
+ADDRESSES_NUM     = 20
+PROPOSAL_NUM      = 10
+ENTITY_NUM        = 10
+RACKING_NUM       = 10
+ISSUE_NUM         = 10
+TIMETABLE_NUM     = 10
+DOCUMENT_NUM      = 10
+
+PROJECT_TYPES = {
+  1 => 'Servicios Sociales',
+  2 => 'Centros de mayores',
+  3 => 'Permanentes',
+  4 => 'Puntuales',
+  5 => 'Entidades',
+  6 => 'Subvencionados',
+  7 => 'Otros'
+}
+
+REQUEST_REASONS = {
+  0 => 'Difusión de proyectos',
+  1 => 'Solicitud voluntarios',
+  2 => 'Publicación actividad en agenda',
+  3 => 'Otros'
+}
+
+AREA_NAMES = [
+  'Derechos Sociales',
+  'Ambiental',
+  'Cultural',
+  'Deportivo',
+  'Educativo',
+  'Socio-sanitario',
+  'Ocio y tiempo libre',
+  'Comunitario y/o de ciudad',
+  'Animales',
+  'Participación',
+  'On line',
+  'Cuidados a las personas',
+  'Sensibilización',
+  'Comunicación',
+  'Cooperación',
+  'Responsabilidad social',
+  'Emergencias',
+  'Otros'
+]
+
+COLLECTIVE_NAMES = [
+  'Menores',
+  'Jóvenes',
+  'Mayores',
+  'Diversidad funcional',
+  'Mujer',
+  'Inmigrantes',
+  'Refugiados',
+  'Personas sin hogar',
+  'Reclusos y exreclusos',
+  'Personas enfermas',
+  'Exclusión social',
+  'Voluntariado',
+  'Toda la población',
+  'Otros'
+]
+
+DISTRICTS = {
+
+  '01' => 'CENTRO',
+  '02' => 'ARGANZUELA',
+  '03' => 'RETIRO',
+  '04' => 'SALAMANCA',
+  '05' => 'CHAMARTIN',
+  '06' => 'TETUAN',
+  '07' => 'CHAMBERI',
+  '08' => 'FUENCARRAL-EL PARDO',
+  '09' => 'MONCLOA-ARAVACA',
+  '10' => 'LATINA',
+  '11' => 'CARABANCHEL',
+  '12' => 'USERA',
+  '13' => 'PUENTE VALLECAS',
+  '14' => 'MORATALAZ',
+  '15' => 'CIUDAD LINEAL',
+  '16' => 'HORTALEZA',
+  '17' => 'VILLAVERDE',
+  '18' => 'VILLA DE VALLECAS',
+  '19' => 'VICÁLAVARO',
+  '20' => 'SAN BLAS',
+  '21' => 'BARAJAS',
+  '22' => 'OTRO MUNICIPIO',
+  '99' => 'OTROS'
+
+}
+
+PROVINCES = {
+
+  '1'  => 'ARABA-ALAVA',
+  '2'  => 'ALBACETE',
+  '3'  => 'ALICANTE-ALACANT',
+  '4'  => 'ALMERIA',
+  '5'  => 'AVILA',
+  '6'  => 'BADAJOZ',
+  '7'  => 'ILLES BALEARS',
+  '8'  => 'BARCELONA',
+  '9'  => 'BURGOS',
+  '10' => 'CACERES',
+  '11' => 'CADIZ',
+  '12' => 'CASTELLON-CASTELLO',
+  '13' => 'CIUDAD REAL',
+  '14' => 'CORDOBA',
+  '15' => 'A CORUÑA',
+  '16' => 'CUENCA',
+  '17' => 'GIRONA',
+  '18' => 'GRANADA',
+  '19' => 'GUADALAJARA',
+  '20' => 'GIPUZKOA',
+  '21' => 'HUELVA',
+  '22' => 'HUESCA',
+  '23' => 'JAEN',
+  '24' => 'LEON',
+  '25' => 'LLEIDA',
+  '26' => 'LA RIOJA',
+  '27' => 'LUGO',
+  '28' => 'MADRID',
+  '29' => 'MALAGA',
+  '30' => 'MURCIA',
+  '31' => 'NAVARRA',
+  '32' => 'OURENSE',
+  '33' => 'ASTURIAS',
+  '34' => 'PALENCIA',
+  '35' => 'LAS PALMAS',
+  '36' => 'PONTEVEDRA',
+  '37' => 'SALAMANCA',
+  '38' => 'SANTA CRUZ DE TENERIFE',
+  '39' => 'CANTABRIA',
+  '40' => 'SEGOVIA',
+  '41' => 'SEVILLA',
+  '42' => 'SORIA',
+  '43' => 'TARRAGONA',
+  '44' => 'TERUEL',
+  '45' => 'TOLEDO',
+  '46' => 'VALENCIA',
+  '47' => 'VALLADOLID',
+  '48' => 'BIZKAIA',
+  '49' => 'ZAMORA',
+  '50' => 'ZARAGOZA',
+  '51' => 'CEUTA',
+  '52' => 'MELILLA'
+
+}
+
+ROAD_TYPES = {
+  'ACCESO'     => '13',
+  'ARROYO'     => '1',
+  'AUTOPISTA'  => '10',
+  'AUTOVIA'    => '364',
+  'AVENIDA'    => '13063',
+  'BULEVAR'    => '199',
+  'CALLE'      => '176374',
+  'CALLEJON'   => '159',
+  'CAMINO'     => '1604',
+  'CAMINOALTO' => '28',
+  'CARRERA'    => '50',
+  'CARRETERA'  => '831',
+  'CAÑADA'     => '107',
+  'COLONIA'    => '364',
+  'COSTANILLA' => '107',
+  'CUESTA'     => '113',
+  'GALERIA'    => '10',
+  'GLORIETA'   => '288',
+  'PARQUE'     => '30',
+  'PARTICULAR' => '21',
+  'PASADIZO'   => '6',
+  'PASAJE'     => '',
+  'PASEO'      => '4239',
+  'PISTA'      => '4',
+  'PLAZA'      => '3478',
+  'PLAZUELA'   => '16',
+  'PUENTE'     => '1',
+  'RONDA'      => '',
+  'TRAVESIA'   => '1007',
+}
+
+PROPOSALS = %w(subvencionado desistido desestimado excluido)
 
 puts "Creando usuario administrador..."
-
 User.first_or_initialize(email: 'admin@madrid.es',
                          password: 'Wordpass1',
-                         password_confirmation: 'Wordpass1')
-    .save!
+                         password_confirmation: 'Wordpass1').save!
 
-  User.first_or_initialize(email: 'user1@madrid.es', password: 'Wordpass1', password_confirmation: 'Wordpass1').save!
+puts "Creando Colectivos"
+AREA_NAMES.each do |name|
+  Area.create!(name: name)
+end
+
+puts "Creando Ámbitos"
+COLLECTIVE_NAMES.each do |name|
+  Collective.create!(name: name)
+end
+
+puts "Creando Propuestas"
+PROPOSALS.each do |name|
+  Proposal.create!(name: name)
+end
+
+puts "Creando Tipos de proyectos"
+PROJECT_TYPES.each do |kind , name|
+  ProjectType.create!(kind: kind)
+end
+
+puts "Creando entidades"
+(1..ENTITIES_NUM).each do |n|
+  Entity.create!(name: "#{Entity.model_name.human} #{n}")
+end
+
+puts "Creando coordinaciones"
+(1..COORDINATIONS_NUM).each do |n|
+  Coordination.create!(name: "#{Coordination.model_name.human} #{n}")
+end
+
+puts "Creando Distritos"
+DISTRICTS.each do |code, name|
+  District.create!(code: code, name: name)
+end
+
+puts "Creando Provincias"
+PROVINCES.each do |code, name|
+  Province.create!(code: code, name: name)
+end
+
+puts "Creando Tipos de vías"
+ROAD_TYPES.each do |name, code|
+  RoadType.create!(name: name, code: code)
+end
+
+puts "Creando Direcciones"
+(1..ADDRESSES_NUM).each do |n|
+  Address.create!(
+    postal_code:           Faker::Address.postcode,
+    road_type:             RoadType.all.sample,
+    road_name:             Faker::Address.street_name,
+    road_number_type:      Address::ROAD_NUMBER_TYPES.sample,
+    road_number:           rand(100).to_s,
+    grader:                Address::GRADERS.sample,
+    stairs:                rand(300).to_s,
+    floor:                 rand(9).to_s,
+    door:                  rand(10).to_s,
+    province:              Province.all.sample,
+    country:               "España",
+    town:                  "Madrid"
+  )
+end
+
+puts "Creando Horarios"
+(1..TIMETABLE_NUM).each do |n|
+  Timetable.create!(
+    day:        Timetable.days.values.sample,
+    start_hour: '11:11',
+    end_hour:   '12:12'
+  )
+end
+
+# puts "Creando Motivos de solicitud"
+# REQUEST_REASONS.each do |kind , name|
+#   RequestReason.create!(kind: kind)
+# end
+
+puts "Creando Proyectos"
+ProjectType.all.each do |project_type|
+  (1..PROJECTS_NUM).each do |n|
+    project = Project.new(
+      name:                  "#{Faker::App.name} #{project_type} #{n}",
+      description:           Faker::Lorem.sentence,
+      functions:             Faker::Lorem.sentence,
+      comments:              Faker::Lorem.sentence,
+      entity_id:             Entity.all.sample.id,
+      execution_start_date:  Faker::Time.between(DateTime.now - 10, DateTime.now),
+      execution_end_date:    Faker::Time.between(DateTime.tomorrow - 10, DateTime.tomorrow),
+      contact_name:          Faker::Name.name,
+      phone_number:          Faker::PhoneNumber.phone_number,
+      email:                 Faker::Internet.email,
+      beneficiaries_num:     10,
+      volunteers_num:        rand(100),
+      project_type:          project_type
+    )
+
+    project.pt_extendable = case project_type.kind
+                              when ProjectType.kinds[:subvention] then PtSubvention.new
+                              when ProjectType.kinds[:entity]     then PtEntity.new
+                            end
+
+    project.save!
+
+    [Address, Area, Collective, Coordination, District, Timetable].each do |model|
+      model.first(5).each do |record|
+        unless project.public_send("#{model.model_name.singular}_ids").include? record.id
+          project.public_send(model.model_name.plural) << record
+        end
+      end
+    end
+  end
+end
