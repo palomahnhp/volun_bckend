3631e9c135a733734256987392c8fd1c696dfca3
jcolomos <sacjavicolomo3@hotmail.com>
2017-01-27 10:19:56 +0100

    Fixed volunteers form and model validations - Issue #3

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -29,11 +29,11 @@ class Volunteer < ActiveRecord::Base
   validates :phone_number_alt, format: { with: /[8|9]\d{8}/ }, allow_blank: true
   validates :id_number, format: { with: /[\w\d]{9}/i }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, allow_blank: true
-  validates :birth_date, inclusion: { in: (150.years.ago..Date.today-1), message: I18n.t('volun_birth_date_error') }
-  validates :availability_date, inclusion: { in: (Date.today+1..Date.today+150.years), message: I18n.t('volun_availability_date_error') }
+  validates :birth_date, inclusion: { in: (150.years.ago..Date.today-1), message: I18n.t('volun_birth_date_error'), allow_blank: true }
+  validates :availability_date, inclusion: { in: (Date.today+1..Date.today+150.years), message: I18n.t('volun_availability_date_error'), allow_blank: true }, unless: ->(o) { o.available == true }
   validates :agreement_date, presence: {message: I18n.t('volun_agreement_date_error')}, if: ->(o) { o.agreement == true }
-  validates :subscribe_date, inclusion: { in: (150.years.ago..Date.today+150.years), message: I18n.t('volun_subscribe_date_error') }
-  validates :unsubscribe_date, inclusion: { in: (150.years.ago..Date.today+150.years), message: I18n.t('volun_unsubscribe_date_error') }
+  validates :subscribe_date, inclusion: { in: (150.years.ago..Date.today+150.years), message: I18n.t('volun_subscribe_date_error'), allow_blank: true }
+  validates :unsubscribe_date, inclusion: { in: (150.years.ago..Date.today+150.years), message: I18n.t('volun_unsubscribe_date_error'), allow_blank: true }
 
   def self.main_columns
     %i(name last_name last_name_alt email gender)
