0b359dbe057b7e6431e75e47766f6dd5003719a7
Andres Leon <andres.leon.job@gmail.com>
2017-01-31 13:25:09 +0100

    Validate that a extendable record cannot be deleted if its parent is referencing it

--- app/models/concerns/pt_commons.rb
+++ app/models/concerns/pt_commons.rb
@@ -8,6 +8,7 @@ module PtCommons
 
     validate :check_project_type
     after_initialize :build_new_project
+    before_destroy :check_project_references
 
     private
 
@@ -25,6 +26,17 @@ module PtCommons
         project_type: ProjectType.where(kind: ProjectType.kinds[self.class.model_name.singular]).take
       )
       build_project(attributes)
+    end
+
+    def check_project_references
+      if has_project_references?
+        errors.add :base, :unable_to_delete_due_to_project_references
+        false
+      end
+    end
+
+    def has_project_references?
+      Project.where(pt_extendable_id: id, pt_extendable_type: self.class.name).exists?
     end
 
   end
