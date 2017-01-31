45cd4ee1d2b8fb2da08be5bdbdd449963e9c65d8
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-01-31 13:12:14 +0100

    Fixing bad merge from master

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -1,8 +1,8 @@
 class Timetable < ActiveRecord::Base
 
-  belongs_to :event
+  belongs_to :event #, required: true
 
-  validates :event_id, :execution_date, :start_hour, :end_hour, presence: true
+  validates :execution_date, :start_hour, :end_hour, presence: true
   validates :start_hour, :end_hour, format: { with: /\A(?:(([01][0-9])|(2[0-4])):([0-5][0-9]))\z/ }
   validate :start_hour_less_than_end_hour
 
