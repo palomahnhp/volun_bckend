c4b60bd3c901e482c0e7b9951d067e7ec5b102f1
Andres Leon <andres.leon.job@gmail.com>
2017-02-07 16:26:28 +0100

    Destroy scaffold of Rt::ProjectPublishing Rt::ProjectUnpublishing Rt::ProjectUnsubscribe Rt::ActivityPublishing Rt::ActivityUnpublishing

--- spec/models/rt/activity_unpublishing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Rt::ActivityUnpublishing, type: :model do
-  let(:activity_unpublishing) { build(:activity_unpublishing) }
-
-  it 'is valid' do
-    expect(activity_unpublishing).to be_valid
-  end
-end
--- spec/models/rt/project_unsubscribe_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Rt::ProjectUnsubscribe, type: :model do
-  let(:project_unsubscribe) { build(:project_unsubscribe) }
-
-  it 'is valid' do
-    expect(project_unsubscribe).to be_valid
-  end
-end
