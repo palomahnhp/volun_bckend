a35781c797a84d29af2ed4a8159a07a2cdc45f03
Andres Leon <andres.leon.job@gmail.com>
2017-02-09 19:17:50 +0100

    Show request form menu

--- app/models/rt/volunteer_subscribe.rb
+++ app/models/rt/volunteer_subscribe.rb
@@ -2,6 +2,17 @@ class Rt::VolunteerSubscribe < ActiveRecord::Base
 
   include RtCommons
 
+  def self.main_attributes
+    %i(
+      name
+      last_name
+      last_name_alt
+      phone_number
+      email
+      notes
+    )
+  end
+
   def to_s
     name
   end
