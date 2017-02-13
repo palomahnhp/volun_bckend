2653fc0505c896bf2be426a8c6f2fbcd6922285a
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-02-13 15:33:21 +0100

    Added validation in Event entity to check if exists at least one timetable. - Issue #6

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,17 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :address
   accepts_nested_attributes_for :timetables, reject_if: :all_blank, allow_destroy: true
 
+	validate :at_least_one_timetable
+
+	private
+
+	def at_least_one_timetable
+		unless timetables.reject(&:marked_for_destruction?).count >= 1
+      errors.add(:base, :one_timetable_at_least)
+      if timetables.any?
+				timetables.first.reload if timetables.first.marked_for_destruction?
+			end
+    end
+  end
+
 end
