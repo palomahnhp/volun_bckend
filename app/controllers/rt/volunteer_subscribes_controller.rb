a074ef8be3945373f68dbf986de014bbb53df0bf
Andres Leon <andres.leon.job@gmail.com>
2017-02-14 11:30:04 +0100

    Refactor status filtering in volunteer_subscribe index view

--- app/controllers/rt/volunteer_subscribes_controller.rb
+++ app/controllers/rt/volunteer_subscribes_controller.rb
@@ -2,14 +2,14 @@ class Rt::VolunteerSubscribesController < ApplicationController
 
   load_and_authorize_resource instance_name: :rt_volunteer_subscribe
 
-  before_action :set_default_statuses, only: :index
+  before_action :set_statuses, only: :index
 
   respond_to :html, :js
 
   def index
     params[:q] ||= Rt::VolunteerSubscribe.ransack_default
     @search_q = @rt_volunteer_subscribes
-                  .with_statuses(RequestForm.statuses.keys.select{|status| cast_as_boolean params[status]})
+                  .with_statuses(statuses.select{|status| params[status]})
                   .search(params[:q])
     @rt_volunteer_subscribes = @search_q.result.paginate(page: params[:page], per_page: params[:per_page]||15)
 
@@ -23,17 +23,17 @@ class Rt::VolunteerSubscribesController < ApplicationController
     end
   end
 
-  def new
-    respond_with(@rt_volunteer_subscribe)
-  end
+  # def new
+  #   respond_with(@rt_volunteer_subscribe)
+  # end
 
   def edit
   end
 
-  def create
-    @rt_volunteer_subscribe.save
-    respond_with(@rt_volunteer_subscribe)
-  end
+  # def create
+  #   @rt_volunteer_subscribe.save
+  #   respond_with(@rt_volunteer_subscribe)
+  # end
 
   def update
     @rt_volunteer_subscribe.update(rt_volunteer_subscribe_params)
@@ -43,6 +43,11 @@ class Rt::VolunteerSubscribesController < ApplicationController
   def destroy
     @rt_volunteer_subscribe.destroy
     respond_with(@rt_volunteer_subscribe)
+  end
+
+  def set_to_processing
+    @rt_volunteer_subscribe.request_form.processing!
+    redirect_to :edit
   end
 
   protected
@@ -63,8 +68,16 @@ class Rt::VolunteerSubscribesController < ApplicationController
 
     alias_method :create_params, :rt_volunteer_subscribe_params
 
-    def set_default_statuses
-      params[:pending]    ||= true
-      params[:processing] ||= true
+    def set_statuses
+      params[:pending]    ||= 'true'
+      params[:processing] ||= 'true'
+
+      statuses.each do |status|
+        params[status] = cast_as_boolean params[status]
+      end
+    end
+
+    def statuses
+      @statuses ||= RequestForm.statuses.keys << :all
     end
 end
