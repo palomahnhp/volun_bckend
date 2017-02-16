1e0aa269a615aa9ec8035fa74604918223e5e961
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-02-16 17:26:05 +0100

    Creating Frontpage elements views - Issue #9

--- config/routes.rb
+++ config/routes.rb
@@ -109,7 +109,6 @@ Rails.application.routes.draw do
 
   get 'maintenance' => 'welcome#maintenance', as: 'maintenance'
   get 'frontpage' => 'frontpage_elements#_index_demo', as: 'frontpage'
-  get 'frontadmin' => 'frontpage_elements#admin_frontpage', as: 'frontadmin'
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
