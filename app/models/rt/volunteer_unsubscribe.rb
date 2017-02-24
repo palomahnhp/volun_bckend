030626436793c63f9dc53e927c139c023f544816
jcolomos <sacjavicolomo3@hotmail.com>
2017-02-24 14:06:18 +0100

    Setting unsubscribe_volunteers views - Issue #13

--- app/models/rt/volunteer_unsubscribe.rb
+++ app/models/rt/volunteer_unsubscribe.rb
@@ -3,6 +3,10 @@ class Rt::VolunteerUnsubscribe < ActiveRecord::Base
   include RtModel
 
   belongs_to :volunteer
+  belongs_to :unsubscribe_level
 
+	def self.main_columns
+    %i(unsubscribe_level notes)
+  end
 
 end
