3db0d38f32a1b304958b28818b616cd0df2995bf
jcolomos <sacjavicolomo3@hotmail.com>
2017-03-03 12:46:32 +0100

    Added translations - Issue #14

--- app/models/volun/availability.rb
+++ app/models/volun/availability.rb
@@ -1,6 +1,6 @@
 class Volun::Availability < ActiveRecord::Base
 
-  enum day: [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
+  enum day: { monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday:7 }
 
   belongs_to :volunteer
 
