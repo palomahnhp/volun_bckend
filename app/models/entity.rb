3f386d80a446281e714a423b5b6691eca8a0789f
Andres Leon <andres.leon.job@gmail.com>
2017-03-13 09:12:04 +0100

    Fix entity creation

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -4,26 +4,24 @@ class Entity < ActiveRecord::Base
 
   belongs_to :reason, class_name: 'Req::Reason', foreign_key: 'req_reason_id'
   belongs_to :entity_type #, required: true
-  belongs_to :address #, required: true
+  belongs_to :address, required: true
   has_one :user, as: :loggable
   has_many :projects, ->(){ order('created_at' => :asc) }
   accepts_nested_attributes_for :projects
+  accepts_nested_attributes_for :address
 
   validates :name, :vat_number, :email, :representative_name, :representative_last_name, :contact_name,
-            :contact_last_name, :entity_type_id,  :address_id, presence: true
+            :contact_last_name, :entity_type_id, presence: true
   validates :phone_number, format: { with: /[6|7|8|9]\d{8}/ }, allow_blank: true
   validates :phone_number_alt, format: { with: /[6|7|8|9]\d{8}/ }, allow_blank: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, allow_blank: true
   validates :vat_number, spanish_vat: true
 
-  def to_s
-    name
+  def self.main_columns
+    %i(name vat_number email entity_type)
   end
 
-  def self.main_columns
-    %i(name
-       vat_number
-       email
-       entity_type)
+  def to_s
+    name
   end
 end
