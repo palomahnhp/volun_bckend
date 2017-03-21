ff5feffc49819aa0194e705530660a74d7d882fb
Andres Leon <andres.leon.job@gmail.com>
2017-03-21 18:30:29 +0100

    Refactor RequestForm and RtModel

--- app/models/concerns/rt_model.rb
+++ app/models/concerns/rt_model.rb
@@ -12,9 +12,7 @@ module RtModel
     default_scope -> { includes(request_form: [:status, :manager]).order('request_forms.status_date desc') }
     scope :with_statuses, ->(statuses = []){
       statuses = [statuses].flatten.compact.select{ |status_name| status_name.to_s.in? status_names }
-      return none unless statuses.any?
-      where('request_forms.rt_extendable_type' => name,
-            'request_forms.req_status_id' => statuses.map{ |status| get_status_id_by_kind(status) })
+      where(request_forms: { rt_extendable_type: name}, req_statuses: { kind: statuses } )
     }
     scope :with_status, ->(status){ with_statuses status }
     scope :pending,     ->(){ with_status(:pending) }
@@ -23,7 +21,7 @@ module RtModel
     scope :rejected,    ->(){ with_status(:rejected) }
 
     class << self
-      delegate :status_names, :get_status_id_by_kind, to: RequestForm
+      delegate :status_names, to: RequestForm
     end
 
     private
