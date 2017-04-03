1a64f7c78e3765781e0341ebd565234bbfd3dd50
Daniel Nacenta <dnacenta2@gmail.com>
2017-04-03 14:05:35 +0200

    Add available days filter

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -9,9 +9,6 @@ class VolunteersController < ApplicationController
     @volunteers = @search_q.result.paginate(page: params[:page], per_page: params[:per_page]||15).with_status(params[:status])
 
     @districts_names = Address.joins(:volunteers).where.not(district: [nil, ""]).all.pluck(:district).uniq
-    @available_days = Volun::Availability.days_i18n.map do |day, translation|
-                        translation
-                      end
 
     @degreeSearch = Degree.filter_by_degree_type_id(params[:dt_id])
     respond_to do |format|
