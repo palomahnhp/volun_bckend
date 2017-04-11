c0792918d9c06d4b7d2ac4d9102773165dae7abb
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-04-11 15:45:01 +0200

    Fix indentation

--- app/assets/javascripts/entity.js
+++ app/assets/javascripts/entity.js
@@ -11,21 +11,21 @@
       }
     }
 		
-		function changeActive() {
-			$("#entity_active").val(!$("#inactive:checkbox:checked").val());
-			if($("#inactive:checkbox:checked").val()) {
-				var d = new Date();
-				var day = d.getDate();
-				var month = d.getMonth() + 1;
-				if (day < 10) {
-					day = "0" + day;
-				}
-				if (month < 10) {
-					month = "0" + month;
-				}
-				var strDate = day + "/" + month + "/" + d.getFullYear();
-				$("#entity_unsubscribed_at").val(strDate);
-			} else {
-				$("#entity_unsubscribed_at").val("");
-			}
-		}
+    function changeActive() {
+      $("#entity_active").val(!$("#inactive:checkbox:checked").val());
+      if($("#inactive:checkbox:checked").val()) {
+        var d = new Date();
+        var day = d.getDate();
+        var month = d.getMonth() + 1;
+        if (day < 10) {
+          day = "0" + day;
+        }
+        if (month < 10) {
+          month = "0" + month;
+        }
+        var strDate = day + "/" + month + "/" + d.getFullYear();
+        $("#entity_unsubscribed_at").val(strDate);
+      } else {
+        $("#entity_unsubscribed_at").val("");
+      }
+    }
