34e8aad77bb51512b04dc121b426c0ed846d5ad1
Andres Leon <andres.leon.job@gmail.com>
2017-04-11 18:52:47 +0200

    Set static manager permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,27 @@ class Ability
 
   def initialize(user)
     return unless user
-
     can :manage, :all
+
+    # case user.manager
+    # manager =
+    case user.loggable
+    when ->(manager){ manager.super_admin? }
+      self.merge Abilities::SuperAdmin.new(user)
+
+    when ->(manager){ manager.admin? }
+      self.merge Abilities::Admin.new(user)
+
+    when ->(manager){ manager.internal_staff? }
+      self.merge Abilities::InternalStaff.new(user)
+
+    when ->(manager){ manager.external_staff? }
+      self.merge Abilities::ExternalStaff.new(user)
+
+    else
+      cannot :manage, :all
+    end
+
   end
 
 end
