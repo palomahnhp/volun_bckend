ed555bdade6c4696c6c328369f1dd913ac5714fb
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-04-11 10:35:15 +0200

    Add validations in activities Add order by 2 columns in timetables

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
 
   belongs_to :eventable, polymorphic: true #, required: true
   belongs_to :address, required: true
-  has_many :timetables, -> { order('timetables.execution_date asc') }, :dependent => :destroy
+  has_many :timetables, -> { order('timetables.execution_date asc, timetables.start_hour asc') }, :dependent => :destroy
   belongs_to :project,  -> { includes(:events).where(events: { eventable_type: Project.name  }) }, foreign_key: 'eventable_id'
   belongs_to :activity, -> { includes(:events).where(events: { eventable_type: Activity.name }) }, foreign_key: 'eventable_id'
 
