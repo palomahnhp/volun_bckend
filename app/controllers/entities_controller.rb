019ad7dbe6de21cd2973837ddfb5ddbb867fa467
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-04-20 15:04:13 +0200

    Add user creation for entities

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -33,6 +33,8 @@ class EntitiesController < ApplicationController
 
   def update
     @entity.update_attributes(entity_params)
+    create_and_assign_user_to_entity!(@entity, params[:entity_notice_type])
+    puts "==================>>>> #{@entity.errors.full_messages}"
     respond_with(@entity)
   end
 
@@ -141,13 +143,20 @@ class EntitiesController < ApplicationController
               :_destroy
             ]
           },
-          {
-            user_attributes: [
-              :id,
-              :notice_type_id,
-              :_destroy
-            ]
-          }
         )
+    end
+
+    def create_and_assign_user_to_entity!(entity, notice_type_id_param)
+      if User.find_by(loggable_type: "Entity", loggable_id: entity.id).nil?
+        user = User.new(login: "user#{'%09d' % entity.id}", loggable: entity, notice_type: NoticeType.find_by(description: notice_type_id_param))
+        user.password = Digest::SHA1.hexdigest("#{entity.created_at.to_s}--#{user.login}")[0,8]
+        user.password_confirmation = user.password
+        user.email = "#{user.login}@volun.es"
+        user.save
+      else
+        user = User.find_by(loggable_type: "Entity", loggable_id: entity.id)
+        user.notice_type_id = NoticeType.find_by(description: notice_type_id_param).id
+        user.save
+      end
     end
 end
