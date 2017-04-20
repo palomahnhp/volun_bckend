eb5249deb925b81082ef6ef6eae0cf71bb44e0f7
Daniel Nacenta <dnacenta2@gmail.com>
2017-04-20 09:38:04 +0200

    Move volunteer index js to assets

--- app/assets/javascripts/volunteer.js
+++ app/assets/javascripts/volunteer.js
@@ -2,6 +2,17 @@
       // Check date fields one time at rendering;
       setAvailDate();
       setAgrmtDate();
+
+      // Clean search values
+      resetButton();
+
+      // Search
+      searchButton();
+      keepSearch();
+
+      //No results alert
+      resultAlert();
+
       // Show others comment fields on traits
       $.each($('[id^="volunteer_assessments_attributes"][id$="trait_id"]'), function( index, value ) {
         showComments(value.id)
@@ -59,4 +70,54 @@
       } else {
         $("#"+otherId).parent().parent().hide();
       }
+    }
+
+    // Clean search values
+    function resetButton() {
+      $("#btn-reset").click(function(){
+          $('#q_address_district_or_address_postal_code_or_name_or_last_name_or_last_name_alt_or_full_name_or_id_number_or_phone_number_or_phone_number_alt_or_email_or_other_academic_info_cont').prop('value', "");
+          $('#q_id_eq').prop('value', "");
+          $("#q_address_district_eq").removeAttr('value');
+          $(".q_availabilities_day_in_all").removeAttr('value');
+          $("#status").removeAttr('value');
+          $('.search-form').submit()
+      });
+    }
+
+    // Search
+    function searchButton() {
+      $("#btn-search").click(function(){
+          $('.q_availabilities_day_in_all').prop('value',$('#availability-select option:selected').val());
+          $('#q_address_district_eq').prop('value', $('#district-select option:selected').val());
+          $('#status').prop('value', $('#status-select option:selected').val());
+          $('.search-form').submit()
+      });
+    }
+
+    function keepSearch() {
+      if ($('#q_address_district_eq').val()) {
+          var districtValue = $('#q_address_district_eq').val()
+          $('#district-select option[value="' + districtValue + '"]').prop('selected', true)
+      };
+
+      if ($('#status').val()) {
+          var statusValue = $('#status').val()
+          $('#status-select option[value="' + statusValue + '"]').prop('selected', true)
+      };
+
+      if ($('.q_availabilities_day_in_all').val()) {
+          var availabilityValue = $('.q_availabilities_day_in_all').val()
+          $('#availability-select option[value="' + availabilityValue + '"]').prop('selected', true)
+      };
+    }
+
+    //No results alert
+    function resultAlert() {
+      if ($('.volun-result').length == 0) {
+          swal(
+              "Lo sentimos",
+              "Su búsqueda no ha encontrado resultados",
+              "error"
+          )
+      };
     }
