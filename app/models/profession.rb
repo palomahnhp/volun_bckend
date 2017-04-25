09a7196b5d82c161b0ac52ac53ad536c61716f6c
Javi Colomo <sacjavicolomo3@hotmail.com>
2017-04-25 11:04:51 +0200

    Add professions to catalog and traslations

--- app/models/profession.rb
+++ app/models/profession.rb
@@ -1,9 +1,14 @@
 class Profession < ActiveRecord::Base
 
+  include Recordable
   include Archivable
 
   validates :name, uniqueness: true
   validates :name, presence: true
+
+  def self.main_columns
+    [:id, :name, :active]
+  end
 
   def to_s
     name
