89a80957ea98f63dbdd46ef53e971dc32e8dc902
Andres Leon <andres.leon.job@gmail.com>
2017-04-28 08:38:25 +0200

    Add mailer configuration for production environment

--- config/environments/preproduction.rb
+++ config/environments/preproduction.rb
@@ -14,10 +14,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # ActionMailer Config
   config.action_mailer.smtp_settings = {
     address: Rails.application.secrets.server_name_mail,
   }
-  # ActionMailer Config
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
